15a501a16be7412404fcaeb56ceea6243f3c10b7
Shayma Mukaram <shaymamukaram@gmail.com>
2018-09-05 20:25:52 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,14 @@
-// Code Product Component Here
+import React, { Component } from "react";
+
+class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    name:"Dunder Mifflin",
+    producer:"PaperCo",
+   hasWatermark:"true",
+   color:"white",
+    weight:210
+    };
+  }
+}
